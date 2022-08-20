151908fa70054978812f0bd9f1e6860c48af0cbb
alexis <alexisrodriguezarteaga@gmail.com>
2022-08-20 14:10:21 -0300

    Create context

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import { useContext } from 'react';    
+import { TaskContext } from '../context/TaskContext'
 
 function TaskCard({task, deleteTask}) {
+
+  const valor = useContext(TaskContext);
   return (
     <div>
       <h1>{task.title}</h1>
